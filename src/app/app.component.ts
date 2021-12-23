1b9322a9b66d0886148c0479ec6389788cb0f2e7
Zara Cooper <zramcooper@gmail.com>
2021-12-24 02:56:03 +0300

    Add cube query properties to app component Add methods to format cube query results

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,179 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EChartsOption } from 'echarts';
+import { map, Observable, reduce, switchMap } from 'rxjs';
+import { CollegeMajorsCubeService, Query } from './college-majors-cube.service';
+
+interface ChartData {
+  xAxisData: string[],
+  seriesData: number[][],
+  seriesLabels: string[]
+}
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'college-majors-dashboard';
+export class AppComponent implements OnInit {
+  selectedGroup = 'total';
+  selectedCategory = 'Biology & Life Science';
+
+  topGroupMajors$!: Observable<EChartsOption>;
+  topCategoryMajors$!: Observable<EChartsOption>;
+
+  majorsPerCategory$ = this.getChartOptions(
+    {
+      'measures': ['WomenStem.count'],
+      'dimensions': ['WomenStem.majorCategory']
+    },
+    'Majors Per Category',
+    'Major Categories',
+    'Number of Majors'
+  );
+
+  majorCategoriesByGender$ = this.getChartOptions(
+    {
+      'measures': ['WomenStem.women', 'WomenStem.men', 'WomenStem.total'],
+      'dimensions': ['WomenStem.majorCategory']
+    },
+    'Graduates per Major Category by Gender',
+    'Major Categories',
+    'Number of Graduates'
+  );
+
+  getTopMajorsInGroup() {
+    this.topGroupMajors$ = this.getChartOptions(
+      {
+        'measures': [
+          `WomenStem.${this.selectedGroup}`
+        ],
+        'order': {
+          [`WomenStem.${this.selectedGroup}`]: 'desc'
+        },
+        'dimensions': [
+          'WomenStem.major'
+        ],
+        'limit': 3
+      },
+      `Popular Majors in ${this.selectedGroup}`,
+      'Major',
+      'Number of Graduates'
+    )
+  }
+
+  getTopMajorsInCategory() {
+    this.topCategoryMajors$ = this.getChartOptions(
+      {
+        'measures': ['WomenStem.women', 'WomenStem.men', 'WomenStem.total'],
+        'order': { 'WomenStem.total': 'desc' },
+        'dimensions': ['WomenStem.major'],
+        'filters': [
+          {
+            'member': 'WomenStem.majorCategory',
+            'operator': 'equals',
+            'values': [this.selectedCategory]
+          }
+        ],
+        'limit': 3
+      },
+      `Graduates in Top 3 Popular Majors in ${this.selectedCategory}`,
+      'Majors',
+      'Number of Graduates'
+    );
+  }
+
+  constructor(private cm: CollegeMajorsCubeService) { }
+
+  ngOnInit() {
+    this.getTopMajorsInGroup();
+    this.getTopMajorsInCategory();
+  }
+
+  private formatBarChartData(title = '', xAxisLabel = '', yAxisLabel = ''): (source$: Observable<ChartData>) => Observable<EChartsOption> {
+    return source$ => source$.pipe(
+      map(chartData => {
+        let options: EChartsOption = {
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'shadow'
+            }
+          },
+          grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+          },
+          title: { text: title, show: true },
+          xAxis: {
+            type: 'category',
+            data: chartData.xAxisData,
+            name: xAxisLabel,
+            axisTick: {
+              alignWithLabel: true
+            }
+          },
+          series: [],
+          yAxis: {
+            type: 'value',
+            name: yAxisLabel
+          },
+          legend: {
+            data: chartData.seriesLabels
+          }
+        };
+
+        chartData.seriesData.forEach((series, index) => {
+          if (options.series && Array.isArray(options.series)) {
+            options.series.push({
+              type: 'bar',
+              data: series,
+              name: chartData.seriesLabels[index],
+              label: {
+                show: true,
+                rotate: 90,
+                align: 'left',
+                verticalAlign: 'middle',
+                position: 'insideBottom',
+                distance: 15,
+                formatter: '{a} → {c}',
+                fontSize: 14,
+              }
+            });
+          }
+        });
+
+        return options;
+      })
+    );
+  }
+
+  private getChartOptions(query: Query, title = '', xAxisLabel = '', yAxisLabel = '') {
+    return this.cm.load(query).pipe(
+      switchMap(data => data),
+      reduce((ac: ChartData, cv: object, index: number) => {
+        const vals = Object.values(cv);
+
+        if (index == 0) {
+          for (let i = 0; i < vals.length - 1; i++) {
+            ac.seriesData.push([]);
+          }
+
+          ac.seriesLabels = Object.keys(cv).slice(1).map(k => k.substring(k.lastIndexOf('.') + 1));
+        }
+
+        ac.xAxisData.push(vals[0]);
+
+        for (let i = 0; i < vals.length - 1; i++) {
+          ac.seriesData[i].push(vals[i + 1]);
+        }
+
+        return ac;
+      },
+        { xAxisData: [], seriesData: [], seriesLabels: [] }
+      ),
+      this.formatBarChartData(title, xAxisLabel, yAxisLabel)
+    )
+  }
 }
